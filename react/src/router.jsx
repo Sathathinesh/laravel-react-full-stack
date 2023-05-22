e45216cb08a89feb3423d21895c016bf54e4a4cb
thinesh1 <thinesh@rhino-partners.com>
2023-05-22 22:28:49 +0530

    fix router

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,20 +1,33 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Signup from "./views/Signup";
 import Login from "./views/Login";
 import Users from "./views/Users";
 import App from "./App";
 import NotFound from "./views/NotFound";
 
-const router = (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/users" component={Users} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />
+    },
+    {
+      path: "/login",
+      element: <Login />
+    },
+    {
+      path: "/signup",
+      element: <Signup />
+    },
+    {
+      path: "/users",
+      element: <Users />
+    },
+    {
+      path: "*",
+      element: <NotFound />
+    }
+  ]
 );
 
 export default router;
